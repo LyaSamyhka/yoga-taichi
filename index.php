ef27698bf116c8dad2759e4841ac55ede6f82ef2
hregnie2 <hregnie2@ADM-INFO003>
2013-09-16 14:59:11 +0200

    first

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Yoga - Taichi</title>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<link rel="stylesheet" href="main.css">
+	</head>
+	<body>
+		<div id="container">
+			<div id="header">
+				<div id="logo"><img src="http://fakeimg.pl/130x130/FCFFE3/?text=logo"></div>
+				<h1>Yoga Taichi 25</h1>
+				<h2>Enseignement traditionnel Tai Chi Chuan - Qigong - Yoga - Méditation</h2>
+			</div>
+			<div id="menu">
+				<span class="lmenu_item vseparator"><a href="#">Nullam mattis pellentesque</a></span>
+				<span class="lmenu_item"><a href="#">Lorem ipsum dolores sit amen dolores sitLorem ipsum dolores sit amen </a></span>
+				<span class="lmenu_item"><a href="#">Lorem ipsum dolores sit amen</a></span>
+				<span class="lmenu_item"><a href="#">Lorem ipsum dolores sit amen</a></span>
+				<span class="lmenu_item"><a href="#">Lorem ipsum dolores sit amen</a></span>
+				<span class="lmenu_item vseparator"><a href="#">Nullam mattis pellentesque ante a sagittis. Phasellus vel justo mauris. </a></span>
+				<span class="lmenu_item active"><a href="#">Lorem ipsum dolores sit amen</a></span>
+				<span class="lmenu_item"><a href="#">Lorem ipsum dolores sit amen</a></span>
+				<span class="lmenu_item"><a href="#">Lorem ipsum dolores sit amen</a></span>
+				<span class="lmenu_item vseparator"><a href="#">Nullam mattis pellentesque</a></span>
+				<span class="lmenu_item"><a href="#">Lorem ipsum dolores sit amen</a></span>
+				<span class="lmenu_item"><a href="#">Lorem ipsum dolores sit amen</a></span>
+				<span class="lmenu_item"><a href="#">Lorem ipsum dolores sit amen</a></span>
+				<span class="lmenu_item"><a href="#">Lorem ipsum dolores sit amen</a></span>
+				<span class="lmenu_item"><a href="#">Lorem ipsum dolores sit amen</a></span>
+			</div>
+		</div>
+	</body>
+</html>
