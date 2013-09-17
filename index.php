0b05fb2766b5ebb8cde3e2b3853db1bd72e20a22
hregnie2 <hregnie2@ADM-INFO003>
2013-09-17 17:23:20 +0200

    third

--- index.php
+++ index.php
@@ -7,36 +7,51 @@
 	</head>
 	<body>
 		<div id="container">
-			<div id="header">
-				<div id="logo"><img src="http://fakeimg.pl/130x130/FCFFE3/?text=logo"></div>
-				<h1>Yoga Taichi 25</h1>
-				<h2>Enseignement traditionnel Tai Chi Chuan - Qigong - Yoga - Méditation</h2>
-			</div>
-                        <div id="bamboo"></div>
-			<div id="menu-left">
+                        <div id="menu-left">
+                                <div id="logo"><img src="http://fakeimg.pl/130x130/FCFFE3/?text=logo"></div>
 				<span class="lmenu_item vseparator"><a href="#">Nullam mattis pellentesque</a></span>
-				<span class="lmenu_item"><a href="#">Lorem ipsum dolores sit amen dolores sitLorem ipsum dolores sit amen </a></span>
-				<span class="lmenu_item"><a href="#">Lorem ipsum dolores sit amen</a></span>
-				<span class="lmenu_item"><a href="#">Lorem ipsum dolores sit amen</a></span>
-				<span class="lmenu_item"><a href="#">Lorem ipsum dolores sit amen</a></span>
+				<span class="lmenu_item"><a href="#">Lorem ipsum dolores dolores sitLorem ipsum dolores </a></span>
+				<span class="lmenu_item"><a href="#">Lorem ipsum dolores</a></span>
+				<span class="lmenu_item"><a href="#">Lorem ipsum dolores</a></span>
+				<span class="lmenu_item"><a href="#">Lorem ipsum dolores</a></span>
 				<span class="lmenu_item vseparator"><a href="#">Nullam mattis pellentesque ante a sagittis. Phasellus vel justo mauris. </a></span>
-				<span class="lmenu_item active"><a href="#">Lorem ipsum dolores sit amen</a></span>
-				<span class="lmenu_item"><a href="#">Lorem ipsum dolores sit amen</a></span>
-				<span class="lmenu_item"><a href="#">Lorem ipsum dolores sit amen</a></span>
+				<span class="lmenu_item active"><a href="#">Lorem ipsum dolores</a></span>
+				<span class="lmenu_item"><a href="#">Lorem ipsum dolores</a></span>
+				<span class="lmenu_item"><a href="#">Lorem ipsum dolores</a></span>
 				<span class="lmenu_item vseparator"><a href="#">Nullam mattis pellentesque</a></span>
-				<span class="lmenu_item"><a href="#">Lorem ipsum dolores sit amen</a></span>
-				<span class="lmenu_item"><a href="#">Lorem ipsum dolores sit amen</a></span>
-				<span class="lmenu_item"><a href="#">Lorem ipsum dolores sit amen</a></span>
-				<span class="lmenu_item"><a href="#">Lorem ipsum dolores sit amen</a></span>
-				<span class="lmenu_item"><a href="#">Lorem ipsum dolores sit amen</a></span>
+				<span class="lmenu_item"><a href="#">Lorem ipsum dolores</a></span>
+				<span class="lmenu_item"><a href="#">Lorem ipsum dolores</a></span>
+				<span class="lmenu_item"><a href="#">Lorem ipsum dolores</a></span>
+				<span class="lmenu_item"><a href="#">Lorem ipsum dolores</a></span>
+				<span class="lmenu_item"><a href="#">Lorem ipsum dolores</a></span>
 			</div>
+                    
                         <div id="menu-right">
                             
                         </div>
-                        
+                    
+			<div id="header">
+				
+				<h1>Yoga Taichi 25</h1>
+				<h2>Enseignement traditionnel Tai Chi Chuan - Qigong - Yoga - Méditation</h2>
+			</div>
+                    
+                        <div id="bamboo"></div>
                         
                         <div id="content">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus at quam a eros malesuada tristique a fringilla ligula. Mauris orci tellus, vulputate a porta et, placerat rhoncus lectus. Etiam vitae mi odio. Ut pharetra ligula id ligula vestibulum convallis non in justo. Mauris varius eget nunc sed consectetur. Nunc magna quam, egestas nec quam ac, facilisis hendrerit dolor. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Maecenas lacinia tempus dui, in sodales nunc mollis vel. Pellentesque id tellus at orci porta viverra.
+
+In hac habitasse platea dictumst. Vivamus sit amet arcu eu leo eleifend sagittis. Sed augue orci, sagittis ac placerat in, viverra pretium lorem. In ac gravida magna. Nullam non risus nulla. Ut pharetra placerat viverra. Cras mi tellus, tincidunt a neque vel, elementum volutpat orci. In blandit velit nisi. Donec nec porta purus. Quisque at neque at mauris luctus consectetur quis in diam. Donec vitae odio consectetur, ullamcorper ante at, fermentum turpis. Etiam ut tortor tristique, venenatis risus convallis, varius dui. Maecenas vestibulum mollis nibh eget rhoncus. Proin eget tellus velit. Integer justo velit, ultrices sit amet arcu non, imperdiet semper mauris. Nunc augue tortor, ornare quis mi vel, luctus ullamcorper quam.
+
+In volutpat, risus quis aliquet mattis, orci augue vestibulum lorem, fringilla gravida purus dui ut magna. Quisque rutrum porta eros, in vehicula odio dignissim vel. Aliquam sed justo nisl. Suspendisse potenti. Nam vitae augue tempus, mollis risus ac, dictum ante. Ut porttitor dolor quis sagittis aliquam. Ut posuere dolor velit, ac rhoncus nibh malesuada et.
+
+Interdum et malesuada fames ac ante ipsum primis in faucibus. Vivamus aliquet dignissim augue ut suscipit. Aenean elementum ipsum ut dui suscipit, eu ultrices ligula dapibus. Proin sit amet varius lacus, vel vestibulum mauris. Sed cursus varius volutpat. Duis arcu est, posuere quis sem ac, aliquam ullamcorper justo. Suspendisse tristique dui tellus, vitae egestas lectus pulvinar imperdiet. Integer nec ipsum non quam interdum auctor. Sed placerat orci massa, dignissim mattis mauris rutrum ut. Integer sit amet arcu purus. Etiam convallis porttitor nisi vel tempus. Quisque sed quam varius arcu faucibus semper in vitae mi. Nullam lacinia, metus sed condimentum blandit, justo erat commodo magna, eget dignissim dolor ligula a elit. Integer tempus varius velit id posuere. Sed gravida dignissim tempus.
+
+Nullam sit amet nunc dui. Vivamus tempor ante id est rhoncus, tempus pharetra sem condimentum. Aliquam a sollicitudin nisi, vel accumsan ligula. Aenean ipsum purus, semper quis ante eget, hendrerit tincidunt nisi. Morbi mattis nisl non libero facilisis scelerisque. Vestibulum sit amet sem non odio vestibulum gravida. Nunc sodales adipiscing lobortis. Sed lorem nisi, ornare nec erat quis, tristique laoreet urna. Sed adipiscing dignissim lorem, tincidunt condimentum enim accumsan sit amet.
                         </div>
+                        
+                        
+                        
 		</div>
             <script type="text/javascript" src="http://code.jquery.com/jquery-2.0.3.min.js"></script>
             <script type="text/javascript" src="main.js"></script>
